7c71e9d49aee02aabc61b2e98ea2b5d638bd5ad9
Mutahhir-Khan <63418479+Mutahhir-Khan@users.noreply.github.com>
2021-01-07 11:26:27 +0500

    order partially done stripe remains

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CheckoutList from '../../Components/CheckoutList/CheckoutList'
 import { totalAmount } from './../../Utility/checkout';
 import { connect } from 'react-redux';
+import OrderForm from './../../Components/OrderForm/OrderForm';
 
 const Checkout = ({total}) => {
+    const [shipFormShown, setshipFormShown] = useState(false)
     return (
         <div>
             <h1>Checkout Page</h1>
             <CheckoutList/>
             <h3>Total Amount {`$${total}`}</h3>
-            
-            <button>PAY NOW </button> --{total}
+            <button onClick={() => setshipFormShown(!shipFormShown)}>PROCEDD & PAY </button> --{total}
+            {shipFormShown && < OrderForm/>}
         </div>
     )
 }
