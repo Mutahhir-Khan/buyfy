25f2b4c4c119af881213572f3270ce73ce6df108
mutahhirkhan <mutahhir199@gmail.com>
2021-04-04 17:54:55 +0500

    test modal integrated

--- src/Pages/Test/Test.jsx
+++ src/Pages/Test/Test.jsx
@@ -1,39 +1,40 @@
-import React, {useState} from 'react'
-import { connect } from 'react-redux';
-import { uploadProduct } from '../../Redux/product/productActions';
-import { uploadImages } from '../../Utility/Utility';
-import Cart from '../../Components/Cart/Cart';
-import Header from './../../Components/Header/Header';
-import Paragraph from './../../Components/Paragraph/Paragraph';
-import Button from './../../Components/Button/Button';
-import ProductCard from './../../Components/ProductCard/ProductCard';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { uploadProduct } from "../../Redux/product/productActions";
+import { uploadImages } from "../../Utility/Utility";
+import Cart from "../../Components/Cart/Cart";
+import Header from "./../../Components/Header/Header";
+import Paragraph from "./../../Components/Paragraph/Paragraph";
+import Button from "./../../Components/Button/Button";
+import ProductCard from "./../../Components/ProductCard/ProductCard";
+import { openModal } from "./../../Redux/modal/modalActions";
 
-const Test = ({uploadProduct, uploadImages}) => {
-    // var [category, setcategory] = useState("")
-    // var [title, settitle] = useState("")
-    // var [cost, setcost] = useState("")
-    // var [description, setdescription] = useState("")
-    // var [quantity, setquantity] = useState("")
-    // var [CoverPhoto, setCoverPhoto] = useState(null)
+const Test = ({ uploadProduct, uploadImages, openModal }) => {
+  // var [category, setcategory] = useState("")
+  // var [title, settitle] = useState("")
+  // var [cost, setcost] = useState("")
+  // var [description, setdescription] = useState("")
+  // var [quantity, setquantity] = useState("")
+  // var [CoverPhoto, setCoverPhoto] = useState(null)
 
-    // var handleSubmit = (e) => {
-    //     e.preventDefault()
-    //     var productObj = {
-    //         category,
-    //         title,
-    //         cost,
-    //         description,
-    //         quantity,
-    //         CoverPhoto,
-    //     }
-    //         // uploadImages(productObj)
-    //     uploadProduct(productObj)
-    // }
-    return (
-        /* 1em = 10px  */
-        <div style={{fontSize: "62.5%"}}> 
-            <h1>Test page</h1>
-            {/* <form onSubmit={handleSubmit}>
+  // var handleSubmit = (e) => {
+  //     e.preventDefault()
+  //     var productObj = {
+  //         category,
+  //         title,
+  //         cost,
+  //         description,
+  //         quantity,
+  //         CoverPhoto,
+  //     }
+  //         // uploadImages(productObj)
+  //     uploadProduct(productObj)
+  // }
+  return (
+    /* 1em = 10px  */
+    <div style={{ fontSize: "62.5%" }}>
+      <h1>Test page</h1>
+      {/* <form onSubmit={handleSubmit}>
                 <input onChange={(e) => setcategory(e.target.value)} type="text" value={category} placeholder="category"/> <br/>
                 <input onChange={(e) => settitle(e.target.value)} type="text" value={title} placeholder="title"/><br/>
                 <textarea onChange={(e) => setdescription(e.target.value)}  value={description} cols="30" rows="10" placeholder="Description"></textarea><br/>
@@ -42,9 +43,9 @@ const Test = ({uploadProduct, uploadImages}) => {
                 <input onChange={(e) => setCoverPhoto(e.target.files[0])}  type="file"  placeholder="cover photo"/>
                 <button>ADD PRODUCT</button>
             </form> */}
-            {/* ------------------------------------------------------------------- */}
-            {/* <Cart/> */}
-            {/* <Header fontWeight="bold" fontSize={32}>This is  my Header</Header>
+      {/* ------------------------------------------------------------------- */}
+      {/* <Cart/> */}
+      {/* <Header fontWeight="bold" fontSize={32}>This is  my Header</Header>
             <Header fontWeight="regular" fontSize={16}>This is  my 2nd  Header</Header>
             <Header fontWeight="regular" fontSize={16} color="darkgreen">This is  my 2nd  Header</Header>
             
@@ -54,12 +55,27 @@ const Test = ({uploadProduct, uploadImages}) => {
 
             <Button  fontWeight="semi-bold" >Click Me! </Button> */}
 
-            {/* <ProductCard /> */}
-        </div>
-    )
-}
+      {/* <ProductCard /> */}
+      <Button
+        onClick={() =>
+          openModal({ modalType: "testModal", modalProps: { number: 2 } })
+        }
+      >
+        open test modal
+      </Button>
+      <Button
+        onClick={() =>
+          openModal({ modalType: "errorModal", modalProps: {error: "ops! </br> something went wrong "} })
+        }
+      >
+        open error modal
+      </Button>
+    </div>
+  );
+};
 var actions = {
-    uploadProduct,
-    uploadImages
-}
-export default connect(null, actions)(Test)
+  uploadProduct,
+  uploadImages,
+  openModal,
+};
+export default connect(null, actions)(Test);
--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,13 @@ import { Router } from "react-router-dom";
 import store from "./Redux/store";
 import { Provider } from "react-redux";
 import history from './History/history';
+import ModalManager from "./Components/ModalManager/ModalManager";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router history ={history}>
       <Provider store={store}>
+        <ModalManager/>
         <App />
       </Provider>
     </Router >
