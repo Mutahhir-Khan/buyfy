2e6c5cc87592929eb15dc7430b23531504267431
mutahhirkhan <mutahhir199@gmail.com>
2021-04-05 13:47:01 +0500

    persistConfig set & persisting auth only

--- src/Redux/rootReducer.js
+++ src/Redux/rootReducer.js
@@ -3,6 +3,15 @@ import authReducer from './authRedux/authReducer';
 import productReducer from './product/productReducer';
 import cartReducer from './cart/cartReducer';
 import modalReducer from './modal/modalReducer';
+import {persistReducer} from "redux-persist"
+import storage from "redux-persist/lib/storage" //default to localStorage to web
+// import sessionStorage from "redux-persist/lib/storage/session" //default to sessionStorage to web
+
+const persistConfig = {
+    key: "root", //at what level we need this persist (we can also separately config the states)
+    storage, //the type of storage we are gonna use
+    whiteList: ['auth'] //these are the state which we have to persist
+}
 
 var rootReducer = combineReducers({
     auth: authReducer,
@@ -12,4 +21,4 @@ var rootReducer = combineReducers({
 })
 
 
-export default rootReducer
+export default persistReducer(persistConfig, rootReducer)
