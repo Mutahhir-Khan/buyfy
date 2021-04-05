aa863b150becd308fd5bc803fac9ef4c7760bdf8
mutahhirkhan <mutahhir199@gmail.com>
2021-04-05 13:46:02 +0500

    persistStore added

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from './rootReducer';
-
+import {persistStore} from "redux-persist"
 
 var middleware = [thunk];
 
@@ -10,5 +10,7 @@ var store = createStore(
     rootReducer,
   composeWithDevTools(applyMiddleware(...middleware))
 );
+
+export const persistor = persistStore(store)
 
 export default store
